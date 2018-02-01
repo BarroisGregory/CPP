371807e6c4ffe47619c18e675dfcc73df08879c1
gregory <gregorybarrois@laposte.net>
2018-02-01 09:16:16 +0100

    vecteur

---
+++ TP1/src/Fibonacci_test.cpp
@@ -0,0 +1,18 @@
+#include "Fibonacci.hpp"
+#include <CppUTest/CommandLineTestRunner.h>
+
+TEST_GROUP(FibonacciTest) { };
+
+TEST(FibonacciTest, test_fibo_recursif) {
+  CHECK_EQUAL(fibonacciRecursif(1), 1);
+  CHECK_EQUAL(fibonacciRecursif(2), 1);
+  CHECK_EQUAL(fibonacciRecursif(3), 2);
+  CHECK_EQUAL(fibonacciRecursif(4), 3);
+}
+
+TEST(FibonacciTest, test_fibo_iteratif) {
+  CHECK_EQUAL(fibonacciIteratif(1), 1);
+  CHECK_EQUAL(fibonacciIteratif(2), 1);
+  CHECK_EQUAL(fibonacciIteratif(3), 2);
+  CHECK_EQUAL(fibonacciIteratif(4), 3);
+}
